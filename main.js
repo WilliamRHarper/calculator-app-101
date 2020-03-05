3b4d017badb55b411fdb5f14d13241b5c9ac40bc
WilliamRHarper <Wrharpe1230@gmail.com>
2020-03-04 23:24:24 -0600

    started editing

--- main.js
+++ main.js
@@ -32,15 +32,20 @@ const subtract = (numA, numB) => {
 const multiply = (numA, numB) => {
   // * to get a product then return it
   // Open up the inspector tool in Chrome and select the Console tab to see what this functions is "logging out" to the console.
-  console.log(numA, numB)
+  const product = numA * numB
+  return product
 }
 
-const divide = null
+const divide = (numA, numB) => {
 // / to get a quotient,
-
-const modulus = null
+  const quotient = numA / numB
+  return quotient
+}
+const modulus = (numA, numB) => {
 // and % to get a remainder.
-
+  const remainder = numA % numB
+  return remainder
+}
 // This function changes the "operation" variable to be equal to the "id" of the button we choose on the web page.
 const changeOperation = (chosenOperation) => {
   operation = chosenOperation
@@ -64,11 +69,11 @@ const equals = () => {
     break;
     case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
     break;
-    case "multiplication": multiply(firstNum, secondNum) 
+    case "multiplication": putResultInElement(multiply(firstNum, secondNum)) 
     break;
-    case "division": console.log(divide(firstNum, secondNum)) 
+    case "division": putResultInElement(divide(firstNum, secondNum)) 
     break;
-    case "modulus": console.log(modulus(firstNum, secondNum)) 
+    case "modulus": putResultInElement(modulus(firstNum, secondNum)) 
     break;
     default: "Choose an operation"
   }
